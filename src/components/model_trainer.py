3180d00db7e823984662bb380b39dcdd75ad4ecc
Mvuyiso <mvuyisogq@gmail.com>
2024-03-26 23:03:31 +0200

    model eval, Flask

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,6 +8,7 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import (RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor, GradientBoostingRegressor)
+from sklearn.model_selection import GridSearchCV
 
 from xgboost import XGBRegressor
 from catboost import CatBoostRegressor
@@ -38,23 +39,59 @@ class ModelTrainer:
             
             logging.info("data loaded - model training has initiated")
 
-            models = {"Linear Regression": LinearRegression(),
-                      "Lasso": Lasso(),
-                      "Ridge": Ridge(),
-                      "K-Neighbors Regressor": KNeighborsRegressor(),
-                      "Decision Tree": DecisionTreeRegressor(),
-                      "Random Forest Regressor": RandomForestRegressor(),
-                      "XGBRegressor": XGBRegressor(), 
-                      "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                      "AdaBoost Regressor": AdaBoostRegressor()
-                      }
+            models = {
+                        "Random Forest": RandomForestRegressor(),
+                        "Decision Tree": DecisionTreeRegressor(),
+                        "Gradient Boosting": GradientBoostingRegressor(),
+                        "Linear Regression": LinearRegression(),
+                        "XGBRegressor": XGBRegressor(),
+                        "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                        "AdaBoost Regressor": AdaBoostRegressor()
+                    }
             
-            parameters = {}
-            
+            parameters ={
+                        "Decision Tree": {
+                            'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                            # 'splitter':['best','random'],
+                            # 'max_features':['sqrt','log2'],
+                        },
+                        "Random Forest":{
+                            # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                         
+                            # 'max_features':['sqrt','log2',None],
+                            'n_estimators': [8,16,32,64,128,256]
+                        },
+                        "Gradient Boosting":{
+                            # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                            'learning_rate':[.1,.01,.05,.001],
+                            'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                            # 'criterion':['squared_error', 'friedman_mse'],
+                            # 'max_features':['auto','sqrt','log2'],
+                            'n_estimators': [8,16,32,64,128,256]
+                        },
+                        "Linear Regression":{},
+                        "XGBRegressor":{
+                            'learning_rate':[.1,.01,.05,.001],
+                            'n_estimators': [8,16,32,64,128,256]
+                        },
+                        "CatBoosting Regressor":{
+                            'depth': [6,8,10],
+                            'learning_rate': [0.01, 0.05, 0.1],
+                            'iterations': [30, 50, 100]
+                        },
+                        "AdaBoost Regressor":{
+                            'learning_rate':[.1,.01,0.5,.001],
+                            # 'loss':['linear','square','exponential'],
+                            'n_estimators': [8,16,32,64,128,256]
+                        }
+                
+                        }
+
             #pass dict into function to be created
             model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, 
                                              X_test=X_test, y_test=y_test,
-                                             models=models)
+                                             models=models,
+                                             params = parameters)
             
             best_model_score = max(sorted(model_report.values()))
 
@@ -72,13 +109,10 @@ class ModelTrainer:
                         obj=best_model)
             
             predicted = best_model.predict(X_test)
-            r2_score = r2_score(y_test, predicted)
+            r2_square = r2_score(y_test, predicted)
 
-
-
-
-
+            return r2_square
 
 
         except Exception as e:
-            pass
+            raise CustomException(e,sys)
